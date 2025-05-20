a96ceb83ad825fc948c64d8bc809376a5725833e
Manish Ranjan <manishranjan.soft@gmail.com>
2025-05-20 18:32:47 +0530

    final

--- app/src/main/java/com/manishsubah/exomediaplayer/MainActivity.kt
+++ app/src/main/java/com/manishsubah/exomediaplayer/MainActivity.kt
@@ -127,19 +127,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    ExoMediaPlayerTheme {
-//        Greeting("Android")
-//    }
-//}
